bb25ca58c89e7f5fe3d965bce778c70ab2d00322
jjbiebe <161864173+jjbiebe@users.noreply.github.com>
2026-02-09 14:02:21 -0600

    Add files via upload

---
+++ Assignment4/Test.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(960, 540);
+  background(144, 238, 144);
+  noStroke();
+  fill(200,0,0);
+  rect(10, 10, 55, 50);
+  fill('rgba(0, 0, 200, 0.5)');
+  rect(30, 30, 55, 50);
+}
